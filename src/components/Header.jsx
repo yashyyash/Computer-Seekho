420bb2e64f58b44e324fe2f682c37c24ff33289b
Yash <yashnil532@gmail.com>
2025-05-16 01:25:50 +0530

    Added Hero Section of Home Page

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+  return (
+    <header className="header">
+      <img src="/Header/vita_logo1.png" alt="VITA logo" />
+      <img src="/Header/vita_logo2.png" alt="Center text" />
+      <img src="/Header/cdac_logo3.png" alt="CDAC logo" />
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/Hero.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Hero.css';
+
+const Hero = () => {
+  return (
+    <div className="hero-container">
+      <img src="/Hero/main1.png" alt="Hero" className="hero-image" />
+    </div>
+  );
+};
+
+export default Hero;
