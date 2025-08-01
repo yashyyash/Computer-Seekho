810adbeea42e451bfd5cf474b588e88c3359e454
Yash <yashnil532@gmail.com>
2025-08-01 23:49:59 +0530

    Added AnnounceMent

---
+++ java-backend/src/main/java/com/seekho/api/repository/AnnouncementRepository.java
@@ -0,0 +1,9 @@
+package com.seekho.api.repository;
+
+import com.seekho.api.entity.Announcement;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AnnouncementRepository extends JpaRepository<Announcement,Integer> {
+}
+
+
